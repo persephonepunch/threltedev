8357b4d680533a26c54f6a91324d981832f7591a
yoonsun lee <yoonsunlee150@gmail.com>
2022-10-05 20:51:36 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Canvas } from "@threlte/core";
+  import Scene from "./Scene.svelte";
+</script>
+
+<div>
+  <Canvas size={{ width: 1280, height: 720 }}>      
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+  }
+</style>
